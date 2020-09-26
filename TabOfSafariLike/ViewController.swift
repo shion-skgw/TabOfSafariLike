dba311c5859fa1855bed54b5f329f401508c122b
Shion Sukegawa <sean.skgw@gmail.com>
2020-09-27 03:43:13 +0900

    Initial Commit

---
+++ TabOfSafariLike/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabOfSafariLike
+//
+//  Created by Shion on 2020/09/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
